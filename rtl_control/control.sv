a5d7bf6155393849e5edf13da7073ab3957ba569
Roland Metivier <metivier.roland@chlorophyt.us>
2020-10-08 22:37:25 -0500

    whoops, rm control

--- rtl_control/control.sv
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-// Control Unit
-module control
-   (input wire logic aclk,
-    input wire logic aresetn,
-
-    // Enable this block
-    input wire logic rx_enable);
-endmodule: control
--- rtl_control/rtl_control_dut.v
+++
@@ -1,8 +0,0 @@
-`timescale 1ns / 1ps
-module control_dut
-   (input wire aclk,
-    input wire aresetn,
-    input wire rx_enable);
-
-    control d(.*); 
-endmodule
